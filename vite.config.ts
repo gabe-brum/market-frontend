e48b6cf2b9ea617e5f19967c1c9fb6fc8673ca04
Gabriel Brum da Luz <52430344+gabe-brum@users.noreply.github.com>
2024-05-10 14:45:22 -0300

    add root files
    add root files

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import type { UserConfig } from "vite";
+import type { InlineConfig } from "vitest";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true,
+    setupFiles: ["./test/setup.ts"],
+    environment: "happy-dom",
+  },
+} as UserConfig & { test: InlineConfig });
